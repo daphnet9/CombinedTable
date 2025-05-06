69bdecacd563afb590d845d9e0a9b9d3cb8e8326
daphnet9 <daphnet9@nycstudents.net>
2025-05-06 01:03:02 +0000

    canSeat method

---
+++ CombinedTable.java
@@ -0,0 +1,12 @@
+public class CombinedTable {
+    private SingleTable one;
+    private SingleTable two;
+    public CombinedTable(SingleTable o, SingleTable t) {
+        one = o;
+        two = t;
+    }
+
+    public boolean canSeat(int num) {
+        return one.getNumSeats() + two.getNumSeats() - 2 >= num;
+    }
+}
---
+++ SingleTable.java
@@ -0,0 +1,26 @@
+public class SingleTable {
+    private int seats;
+    private double viewQuality;
+    private int height;
+    public SingleTable(int s, double v, int h) {
+        seats = s;
+        viewQuality = v;
+        height = h;
+    }
+    
+    public int getNumSeats() {
+        return seats;
+    }
+
+    public int getHeight() {
+
+    }
+
+    public double getViewQuality() {
+        
+    }
+
+    public double setViewQuality(double value) {
+
+    }
+}
